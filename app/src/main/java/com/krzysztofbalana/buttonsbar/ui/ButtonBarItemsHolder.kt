19373f51314ab443583da3e06e4a2f774a36b06c
Krzysztof Balana <krzysztof.balana@esky.com>
2019-01-22 23:31:04 +0100

    Add viewsholder children measuring logic

--- app/src/main/java/com/krzysztofbalana/buttonsbar/ui/ButtonBarItemsHolder.kt
+++ app/src/main/java/com/krzysztofbalana/buttonsbar/ui/ButtonBarItemsHolder.kt
@@ -7,4 +7,34 @@ import android.widget.LinearLayout
 class ButtonBarItemsHolder @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ): LinearLayout(context, attrs, defStyleAttr) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val desiredWidth = MeasureSpec.getSize(widthMeasureSpec)
+
+        val (totalWidth, totalHeight) = recalculateChildren(desiredWidth, widthMeasureSpec, heightMeasureSpec)
+
+        setMeasuredDimension(totalWidth, totalHeight)
 }
+
+    private fun recalculateChildren(desiredWith: Int, widthMeasureSpec: Int, heightMeasureSpec: Int): Pair<Int, Int> {
+        val (totalWidth, totalHeight) = measureChildrensActualDimensions(widthMeasureSpec, heightMeasureSpec)
+
+    }
+        return Pair(totalWidth, totalHeight)
+    }
+
+    private fun measureChildrensActualDimensions(widthMeasureSpec: Int, heightMeasureSpec: Int): Pair<Int, Int> {
+        var totalWidth = 0
+        var totalHeight = 0
+
+        viewChildren<ButtonBarItem>().forEach {
+            measureChild(it, widthMeasureSpec, heightMeasureSpec)
+            totalWidth += it.measuredWidth
+            if (it.measuredHeight > totalHeight) {
+                totalHeight = it.measuredHeight
+            }
+        }
+        return Pair(totalWidth, totalHeight)
+    }
+}
+
+fun <T> ViewGroup.viewChildren(): List<T> = (0 until this.childCount).map { this.getChildAt(it) as T }
